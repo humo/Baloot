2ea7110db577410342165780198e2c1634388668
مشاري العتيبي <a1@macbook-air-alkhas-b-1.local>
2018-09-15 12:42:41 +0300

    Initial Commit

---
+++ Balot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Balot
+//
+//  Created by 1 on 05/01/1440 AH.
+//  Copyright © 1440 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
